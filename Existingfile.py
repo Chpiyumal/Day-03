85f2c5656c6ac840de8b2a225d17c7885523cc38
chpiyumal <kmchpiyumal@gmail.com>
2025-06-25 16:09:55 +0530

    path dir

--- Existingfile.py
+++ Existingfile.py
@@ -7,3 +7,13 @@ path_to_check_dir = "C:/Users/Student_15/Desktop/Python Chp/Codes/Day 03"
 with open(path_to_check_file, 'w') as f:
     f.write("Hello")
 os.makedirs(path_to_check_dir, exist_ok=True)
+
+if os.path.exists(path_to_check_file):
+    print(f"'{path_to_check_file} exists")
+else:
+    print(f"{path_to_check_file} does not exist")
+
+if os.path.exists(path_to_check_dir):
+    print(f"{path_to_check_dir} exists")
+else:
+    print(f"{path_to_check_dir} does not exist") 
